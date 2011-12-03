020779f40fa9ca8ba4dd676cd2c7b2101cc50ed0
Alex Wardi <aaw5017@gmail.com>
2011-12-03 17:22:14 -0500

    Made changes to draw() function

--- toggles.cpp
+++ toggles.cpp
@@ -12,14 +12,17 @@ toggle::toggle(double xx,double yy)
 
 void toggle::draw()
 {
-      glPushMatrix();
-      glTranslated(x*20,0,y*20);
-      glRotated(angle, 0, 1, 0);
-      glScaled(0.25,0.25,0.25);
-      glTranslated(0,1,0);
-      glColor3f(1,1,0);
-      glutSolidTeapot(1);
+    glPushMatrix();
+        glTranslated(x*20,0,y*20);
+        glScalef(0.25,1.5,0.25);
+        glutSolidCube(1.0);
       glPopMatrix();
 
-  
+    glPushMatrix();
+      glColor3f(1,0,0);
+      glTranslated(x*20, 0.78, y*20);
+      glColor3f(1,0,0);
+      glScalef(0.15,0.15,0.15);
+      glutSolidCube(0.5);
+    glPopMatrix(); 
 }
